f600e084153e35b59355aa491ddd34b690513aef
wser <w.web.dev@gmail.com>
2018-03-28 09:55:56 +0200

    new deploy method

--- docs/sw.js
+++ docs/sw.js
@@ -12,20 +12,20 @@ workboxSW.precache([
     "revision": "870039d9d28080b1f54948700604daa7"
   },
   {
-    "url": "/nuxtpwa/_nuxt/layouts_default.2c60b406deca6ec59415.js",
-    "revision": "2c898314b8867d248f12bc43557397ad"
+    "url": "/nuxtpwa/_nuxt/layouts_default.5d9739528a9274e04a6a.js",
+    "revision": "8c40216f6bf84c5c447bdbedabaa7b65"
   },
   {
-    "url": "/nuxtpwa/_nuxt/manifest.e2acf594ff57fac9d3a1.js",
-    "revision": "68dd9738c4eab76fcc5f9d0666e356d9"
+    "url": "/nuxtpwa/_nuxt/manifest.0fc69764bca5a58517c3.js",
+    "revision": "235870b5ca0f37359842c08f94d518d9"
   },
   {
     "url": "/nuxtpwa/_nuxt/pages_about.dce09883de8fb76a8a11.js",
     "revision": "92a590dfaf52a6d6506aedc59bbbfb51"
   },
   {
-    "url": "/nuxtpwa/_nuxt/pages_index.ce8a79b998c309895b05.js",
-    "revision": "122793e5a9a8c5b147684a94cc213a99"
+    "url": "/nuxtpwa/_nuxt/pages_index.d09a51222deae83427be.js",
+    "revision": "e60b3438e13177450f65e696de5d74d7"
   },
   {
     "url": "/nuxtpwa/_nuxt/vendor.5a873d15ce0ab2e9cdaa.js",
