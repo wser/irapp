bcf74de4c31953abe8e717960c4f565803f7d07a
wser <w.web.dev@gmail.com>
2018-04-24 09:44:49 +0200

    fixed router-auth

--- docs/sw.js
+++ docs/sw.js
@@ -8,8 +8,8 @@ const workboxSW = new self.WorkboxSW({
 
 workboxSW.precache([
   {
-    "url": "/_nuxt/app.fcc51a00f500c2ba3f2e.js",
-    "revision": "f34ec74d28d81ff69ce551efd20fe34a"
+    "url": "/_nuxt/app.f9be0a46e6bdfdffc61c.js",
+    "revision": "7bf7103a19fe8b1a251874522ba35443"
   },
   {
     "url": "/_nuxt/app.feaeee5018f85b1652aa15fc93e131fb.css",
@@ -20,40 +20,40 @@ workboxSW.precache([
     "revision": "c1117f795e45f30844d14afa1ba578e0"
   },
   {
-    "url": "/_nuxt/manifest.9c97f624051ff9f7bb2a.js",
-    "revision": "d4084c5da19dd7ed6afc39ce8473083f"
+    "url": "/_nuxt/manifest.4face5136997c631c1e9.js",
+    "revision": "f90f3a35fd2bb2856e18a04fd08aa1c8"
   },
   {
     "url": "/_nuxt/pages_dashboard_index.591bf7f01281fa0450d2.js",
     "revision": "4e1c21e1bfcc66c37223e43ead3e9ad9"
   },
   {
-    "url": "/_nuxt/pages_dashboard_items.a7d162bc12f77b07f345.js",
-    "revision": "206924867b75cceace4f18a719328477"
+    "url": "/_nuxt/pages_dashboard_items.37887f644c1f8dd3b070.js",
+    "revision": "5ec979e5826b99ab4bc4bcf76307f6ae"
   },
   {
-    "url": "/_nuxt/pages_dashboard_obrasci.9334706eadaa8ea4ef82.js",
-    "revision": "be5c4fd27dbd7d55194850d3cc612eb3"
+    "url": "/_nuxt/pages_dashboard_obrasci.552c60a6e8e3c89ae657.js",
+    "revision": "58148cf21602c68644ba6349516332b8"
   },
   {
-    "url": "/_nuxt/pages_dashboard_planiranje.a65c3225f32030afeeb9.js",
-    "revision": "d0c773676d832cdcd7b58baf0b88f17c"
+    "url": "/_nuxt/pages_dashboard_planiranje.33cde28428fcd21c2f58.js",
+    "revision": "433f639ce9462be97421c8e977524bea"
   },
   {
     "url": "/_nuxt/pages_dashboard_settings.6d04c513bb0eb82fed93.js",
     "revision": "6063af296394c83d427214a6f42e2d4f"
   },
   {
-    "url": "/_nuxt/pages_dashboard.0d7994439a312b6d7795.js",
-    "revision": "629cc3fe01c608d44717b097687c7e7d"
+    "url": "/_nuxt/pages_dashboard.85cd95a6277cf2e31a47.js",
+    "revision": "64bea4be74d72177765e52495e945e76"
   },
   {
     "url": "/_nuxt/pages_index.369910f0d635f39b9abe.js",
     "revision": "837748eb08a0547074a016d5479fe5a9"
   },
   {
-    "url": "/_nuxt/pages_login.1c12cb7282c59cdad8e5.js",
-    "revision": "7cdd53f3b74d9e38b294ee968073d19e"
+    "url": "/_nuxt/pages_login.f71834cd542bac45f44f.js",
+    "revision": "2f14b8fbd915229f91cf327e072ccdb9"
   },
   {
     "url": "/_nuxt/vendor.5c5255891656f2aff902.js",
--- middleware/router-auth.js
+++ middleware/router-auth.js
@@ -3,12 +3,12 @@ const routerPrefix = process.env.DEPLOY_ENV === 'GH_PAGES'
                  : ''
 
 export default function ({ store, redirect, route }) {
-  store.state.user != null && route.name == 'login' ? redirect(routerPrefix+'/admin') : ''
+  store.state.user != null && route.name == 'login' ? redirect(routerPrefix+'/dashboard') : ''
   store.state.user == null && isAdminRoute(route) ? redirect(routerPrefix+'/login') : ''
 }
 
 function isAdminRoute(route) {
-  if (route.matched.some(record => record.path == routerPrefix+'/admin')) {
+  if (route.matched.some(record => record.path == routerPrefix+'/dashboard')) {
     return true
   }
 }
